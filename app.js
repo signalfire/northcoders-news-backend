9108f576091f9df06be5e14c8d898bef93830c97
Robert Coster <rob@signalfire.co.uk>
2018-09-20 07:42:49 +0100

    Added DELETE as option for CORS

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('view engine', 'ejs');
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, PATCH, OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, PATCH, DELETE, OPTIONS');
 
     next();
 });
